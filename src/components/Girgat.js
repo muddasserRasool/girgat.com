c8db7fff4643c788bbe71b43ac5e8bf4ff200348
muddasserRasool <69166432+muddasserRasool@users.noreply.github.com>
2020-09-30 19:19:01 +0300

    Pictures Fixed

--- src/components/Girgat.js
+++ src/components/Girgat.js
@@ -3,7 +3,7 @@ import TypistAnimate from './Typist';
 import { Link } from "react-router-dom";
 import useWebAnimations, { bounce} from "@wellyshen/use-web-animations";
 import TwoContainers from './twocontainers';
-import Pic1 from './Media/1.jpg';
+import SingleLineGridList from './Swiper';
 
 
 
@@ -23,45 +23,39 @@ export default function Girgat() {
   }, [ArrowMove]);
 
   return (
-    <div className="MainBody">
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+    <div >
       <br />
       <span>
       <h4 className="Hi">HI, WE AT GIRGAT.COM</h4>
-      <h1 className="help">We're here to Make your</h1>
+      <h1 className="help">We're here to Make your &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
       <TypistAnimate />
-
+      </span>
       <br />
       <br />
+      <span>
       <ul className="how-we-work" id="icon" ref={ArrowMove.ref}>
         <li>
           <Link style={{ textDecoration: "none" }} to="/Aboutus">
-            <span  className="Newbutton">Lets Get in Contact!</span></Link>
+            <span  className="Newbutton">Lets Get in Contact! </span></Link>
         </li>
       </ul>
       </span>
-      <span>
-        <img className="Pictures" src={Pic1} width="25%" height="25%" alt="1"/>
-      </span>
       <br />
       <br />
       <br />
       <br />
       <br />
       <br />
+      <SingleLineGridList /> 
+      
+      
+      {/* <span>
       <br />
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+        <img className="Pictures" src={Pic1} width="35%" height="35%" alt="1"/>
+      </span> */}
+      {/* <SwiperExample /> */}
+      
       <br />
       <TwoContainers />
       <br />
---
+++ src/components/tileData.js
@@ -0,0 +1,82 @@
+import image1 from './Media/1.jpg';
+import image2 from './Media/2.jpg';
+import image3 from './Media/3.jpg';
+import image4 from './Media/4.jpg';
+import image5 from './Media/5.jpg';
+import image6 from './Media/6.jpg';
+import image7 from './Media/7.jpg';
+import image8 from './Media/8.jpg';
+import image9 from './Media/9.jpg';
+import image10 from './Media/10.jpg';
+import image11 from './Media/11.jpg';
+import image12 from './Media/12.jpg';
+ 
+
+ const tileData = [
+  {
+      img: image1,
+      title: 'Image1',
+      author: 'author1',
+    },
+    {
+        img: image2,
+        title: 'Image2',
+        author: 'author2',
+      },
+      {
+        img: image3,
+        title: 'Image3',
+        author: 'author3',
+      },
+      {
+        img: image4,
+        title: 'Image4',
+        author: 'author4',
+      },
+      {
+        img: image5,
+        title: 'Image5',
+        author: 'author5',
+      },
+      {
+        img: image6,
+        title: 'Image6',
+        author: 'author6',
+      },
+      {
+        img: image7,
+        title: 'Image7',
+        author: 'author7',
+      },
+      {
+          img: image8,
+          title: 'Image8',
+          author: 'author8',
+        },
+        {
+          img: image9,
+          title: 'Image9',
+          author: 'author9',
+        },
+        {
+          img: image10,
+          title: 'Image10',
+          author: 'author10',
+        },
+        {
+          img: image11,
+          title: 'Image11',
+          author: 'author11',
+        },
+        {
+          img: image12,
+          title: 'Image12',
+          author: 'author12',
+        },
+     
+   
+  ];
+
+  
+export default tileData
+ 
